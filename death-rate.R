67b69e87cf8168613117b34da9d9fc0854afe759
Casey Labrack <clabrack@gmail.com>
2019-02-27 11:37:53 -0500

    adding caption

--- death-rate.R
+++ death-rate.R
@@ -3,6 +3,8 @@ library(grid)
 
 source("deth.R")
 
+dev.off()
+
 data = read_tsv("by-age.tsv") %>%
   select(3:5) %>% 
   rename(Age = `Single-Year Ages Code`) %>%
@@ -13,18 +15,18 @@ data = read_tsv("by-age.tsv") %>%
 p = ggplot(data, aes(Age,Rate)) +
   geom_area(fill = hcl(60,25,85)) +
   scale_x_continuous(expand = c(0,0)) +
-  scale_y_continuous(expand = c(0,0), limits = c(0,.08),
+  scale_y_continuous(expand = c(0,0), limits = c(0,.085),
                      labels = function (x) { x * 100 }, breaks = c(0,.01,.02,.04,.08)) +
-  ggtitle("The Beginning of the End", subtitle = "Deaths per Hundred People by Age") +
+  labs(title = "The Beginning of the End", subtitle = "Deaths per Hundred People by Age",
+       caption = 'Data: "Underlying Cause of Death 1999-2017," WONDER Online Database, Centers for Disease Control and Prevention.') +
   theme(panel.grid.minor = element_blank(),
         axis.title.y = element_blank(),
         panel.grid.major.x = element_blank())
 
 g = ggplotGrob(p)
-
 g$layout$l[g$layout$name == "title"] <- 2
 g$layout$l[g$layout$name == "subtitle"] <- 2
+g$layout$l[g$layout$name == "caption"] <- 2
+grid.draw(g)
 
-# grid.draw(g)
-
-ggsave("_export/rate.png", plot = g, width = 5, height = 3, dpi = 300, units = "in")
+ggsave("_export/rate.png", plot = g, width = 4.5, height = 3, dpi = 300, units = "in")
--- deth.R
+++ deth.R
@@ -1,10 +1,12 @@
 library(tidyverse)
 
 theme_set(
-  theme_minimal() +
+  theme_minimal(base_size = 9) +
     theme(
       plot.title = element_text(face = "bold"),
       plot.subtitle = element_text(margin = margin(t = 0, r = 0, b = 24, l = 0, unit = "pt"),
-                                   face = "italic")
+                                   face = "italic"),
+      plot.caption = element_text(hjust = 0, size = rel(.6),
+                                  margin = margin(t = 12, r = 0, b = 0, l = 0, unit = "pt"))
     )
 )
